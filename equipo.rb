caac574397ecc4e4081a4f87ae1323d75c33e05b
ropema <ropema@misena.edu.co>
2021-09-09 21:14:53 -0300

    Change class equipo

--- equipo.rb
+++ equipo.rb
@@ -2,13 +2,14 @@ require_relative 'participante'
 class Equipo
     attr_accessor :codigo, :nombre, :puntaje_total, :participantes
     def initialize(codigo,nombre)
-    	@codigo=codigo;
+        @codigo=codigo;
         @nombre=nombre;
         @participantes=Array.new;
         @puntaje_total=0;
     end
     def cargar_participante(codigo,nombre,edad,dni,cinturon,partidas_ganadas,partidas_empatadas,partidas_perdidas)
-    	participante=Participante.new(codigo,nombre,edad,dni,cinturon,partidas_ganadas,partidas_empatadas,partidas_perdidas);
-    	@participantes.push(participante);
+        participante=Participante.new(codigo,nombre,edad,dni,cinturon,partidas_ganadas,partidas_empatadas,partidas_perdidas);
+        @participantes.push(participante);
+        @puntaje_total=@puntaje_total+participante.puntaje;
     end
 end
--- equipo_spec.rb
+++ equipo_spec.rb
@@ -13,23 +13,20 @@ RSpec.describe "Equipo" do
         end
         it '1.3 Variable Instance nombre' do
             expect(@equipo.nombre).to eq('rojo')
-        end                
+        end
     end
     context '2 Load of participantes' do
         it 'Load one participante' do
             equipo=Equipo.new('E01','rojo')
             participante=Participante.new('001','Juan Cisternas','25','17650788-6','cafe',3,2,2)
             equipo.cargar_participante('001','Juan Cisternas','25','17650788-6','cafe',3,2,2)
-            #Solution 1
             expect(equipo.participantes[0].codigo).to eq('001')
-
-            #Solution 2
-            #expect(@enterprise.workers[0]).to have_attributes(firtsName: 'Richar')
-
-            #Solution 3
-            # expect(@enterprise.workers).to match_array([
-            #     have_attributes(code: '0001', firtsName: 'Richar'),
-            # ])
+        end
+        it 'Change Variable Instance puntaje_total' do
+            equipo=Equipo.new('E01','rojo')
+            participante=Participante.new('001','Juan Cisternas','25','17650788-6','cafe',3,2,2)
+            equipo.cargar_participante('001','Juan Cisternas','25','17650788-6','cafe',3,2,2)            
+            expect(equipo.puntaje_total).to eq(18)
         end
     end
 end
